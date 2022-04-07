43c929b81c808c4f7a800257f3f9ba54c892abca
Mark Weiss <weisstmark@gmail.com>
2022-04-07 03:36:52 -0600

    Clearing database after game end so a second game can be played

--- app/src/main/java/edu/sdsmt/group2/Control/EndGameActivity.java
+++ app/src/main/java/edu/sdsmt/group2/Control/EndGameActivity.java
@@ -1,19 +1,32 @@
 package edu.sdsmt.group2.Control;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import edu.sdsmt.group2.Model.Cloud;
 import edu.sdsmt.group2.R;
 
 public class EndGameActivity extends AppCompatActivity {
     public final static String PLAYER1_MESSAGE = "edu.sdsmt.group2.PLAYER1_MESSAGE";
     public final static String PLAYER2_MESSAGE  = "edu.sdsmt.group2.PLAYER2_MESSAGE";
     public final static String WINNER_MESSAGE  = "edu.sdsmt.group2.WINNER_MESSAGE";
+    private final DatabaseReference gameRef = FirebaseDatabase.getInstance().getReference().child("game");
     TextView player1;
     TextView player2;
     TextView winner;
+    private Activity back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,7 @@ public class EndGameActivity extends AppCompatActivity {
 
     public void onReturnClick(View view)
     {
+        gameRef.removeValue();
         finish();
     }
 }
